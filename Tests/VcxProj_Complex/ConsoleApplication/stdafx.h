d37effd49466ac373555b4c98c7bd42582eb9d90
roozbeh@homewin10.com <roozbeh@homewin10.com>
2018-04-28 22:32:06 -0600

    added LinuxDebug and LinuxRelease configs

--- Tests/VcxProj_Complex/ConsoleApplication/stdafx.h
+++ Tests/VcxProj_Complex/ConsoleApplication/stdafx.h
@@ -5,10 +5,14 @@
 
 #pragma once
 
-#include "targetver.h"
+//#include "targetver.h"
 
 #include <stdio.h>
-#include <tchar.h>
+
+
+#ifndef Unix
+	#include <tchar.h>
+#endif
 
 
 
--- Tests/VcxProj_Complex/DynamicLib/DynamicLib.h
+++ Tests/VcxProj_Complex/DynamicLib/DynamicLib.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#if defined(__GNUC__)
 #ifdef DYNAMICLIB_EXPORTS  
-#define MATHLIBRARY_API __declspec(dllexport)   
+#define MATHLIBRARY_API __attribute__((visibility("default")))   
 #else  
-#define MATHLIBRARY_API __declspec(dllimport)   
+#define MATHLIBRARY_API 
 #endif 
+#else
+	#ifdef DYNAMICLIB_EXPORTS  
+	#define MATHLIBRARY_API __declspec(dllexport)   
+	#else  
+	#define MATHLIBRARY_API __declspec(dllimport)   
+	#endif 
+#endif
+
 
 MATHLIBRARY_API void PrintfFromDynamicLib();
