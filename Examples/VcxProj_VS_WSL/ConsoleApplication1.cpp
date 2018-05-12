57b6a4d5ebde0dd3b7df1a7e3a1f1ec2ca58b7da
roozbeh@homewin10.com <roozbeh@homewin10.com>
2018-05-12 13:10:42 -0600

    add an example for using it in visual studio and also using WSL

---
+++ Examples/VcxProj_VS_WSL/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+
+int main()
+{
+    printf("salam\n");
+    return 0;
+}
+
---
+++ Examples/VcxProj_VS_WSL/Source.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "stdio.h"
+
+void test1()
+{
+	printf("salam\n");
+}
